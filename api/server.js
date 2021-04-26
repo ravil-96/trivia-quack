803c1d55277d2dccd1d15a9d5ca7acbcb234f2c1
Elwin <elwin.carlos09@gmail.com>
2021-04-26 11:17:24 +0100

    create game server

--- api/server.js
+++ api/server.js
@@ -4,7 +4,40 @@ const cors = require('cors');
 const server = express();
 server.use(cors());
 server.use(express.json());
-
+// Set up root route
 server.get('/', (req, res) => res.send('Welcome'));
+
+// Game server setup
+const gameServer = require("http").createServer(server);
+const gameRoutes = require('./mvc/routes/gameRoutes');
+gameServer.use('/games', gameRoutes);
+    // Integrate http server with new instance of socket.io
+const io = require('socket.io')(gameServer, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+// Add socket connection
+io.on('connection', socket => {
+
+    socket.on('create', (roomId) => {
+        console.log(`Room with id ${roomId} has been created!`);
+        socket.join(roomId);
+        io.to(roomId).emit('admin-message', `${socket.id} has joined`);
+        io.to(roomId).emit('count', io.sockets.adapter.rooms.get(roomId) ? io.sockets.adapter.rooms.get(roomId).size : 0);
+        socket.on('new-message', ({ username, message }) => {
+            io.in(roomId).emit('incoming-message', { username, message })
+        });
+        // *******************************************************************************************************************
+        // HANDLE FOR NEW USER ENTERING ROOM
+        socket.on("disconnect", () => {
+            io.to(roomId).emit('count', io.sockets.adapter.rooms.get(roomId) ? io.sockets.adapter.rooms.get(roomId).size : 0);
+            io.to(roomId.emit('admin-message', `${socket.id} has left`))
+        });
+    });
+
+});
 
 module.exports = server
