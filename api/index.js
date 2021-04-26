3f942a8e984fd297536c0ac8f13d3cecbc4a982f
Elwin <elwin.carlos09@gmail.com>
2021-04-26 11:16:28 +0100

    add listener for game server and host on port 5001

--- api/index.js
+++ api/index.js
@@ -1,5 +1,6 @@
 const app = require('./server');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5001; // Hard code game server port
 
-app.listen(port, () => console.log(`Express now departing from port ${port}!`))
+app.listen(port, () => console.log(`Express now departing from port ${port}!`));
+app.listen(port, () => console.log(`Open for play on port ${port}!`));
