8fffedc0ee15656a7c5894196a0540cc11dcbf37
HeyAero <aeroyts@icloud.com>
2021-04-26 12:59:06 +0100

    add CreateForm component, update imports and add import into Create page

---
+++ client/src/components/CreateForm/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CreateForm = () => {
+  return (
+    <form>
+
+    </form>
+  );
+}
+
+export default CreateForm;
--- client/src/pages/Create/index.js
+++ client/src/pages/Create/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { CreateForm } from '../../components';
 
 const Lobby = () => {
   return (
     <main id="create" className="container">
       <h1>Create</h1>
+      <CreateForm />
     </main>
   );
 };
