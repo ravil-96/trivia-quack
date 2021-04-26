67f25d55abed22a55838b46630dc25d80d8cfb23
HeyAero <aeroyts@icloud.com>
2021-04-26 12:01:42 +0100

    add import for bootstrap, add game buttons on homepage and add logo file

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 
 const Homepage = () => {
   return (
-    <main id="homepage">
+    <main id="homepage" className="container">
       <h1>Trivia Quack</h1>
       <img className="planet-left" src="../../images/planet-1.png" alt="Blue Planet" />
       <img className="planet-right" src="../../images/planet-2.png" alt="Orange Planet" />
+      <div className="text-center">
+      <a className="new-game-button">New Game</a>
+      <a className="join-game-button">Join Game</a>
+      </div>
     </main>
   );
 };
