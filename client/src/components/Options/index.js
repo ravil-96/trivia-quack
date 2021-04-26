03c7eb34b7576a5caf75906a59219bc6d131fa93
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-26 18:07:53 +0100

    adding Options component and starting to write some functionality

--- client/src/components/Options/index.js
+++ client/src/components/Options/index.js
@@ -3,16 +3,31 @@ import React, {useState} from 'react';
 
 function Options () {
 
+// 1. The user clicks on a button and it is highlighted
+// 2. The user submits an answer
 
+    const [selectedOption, setSelectedOption] = useState;
+
+    const handleSelect = e => {
+        input = e.currentTarget
+        setSelectedOption = input
+    }
+
+    const handleSubmit = e => {
+        
+        
+    }
 
   return (
     <>
-      <button>Option1</button>
-      <button>Option2</button>
-      <button>Option3</button>
-      <button>Option4</button>
+      <button onClick={handleSelect} value='selected'>Option1</button>
+      <button onClick={handleSelect} >Option2</button>
+      <button onClick={handleSelect}>Option3</button>
+      <button onClick={handleSelect}>Option4</button>
     </>  
 
   )
 
 }
+
+export default Options;
