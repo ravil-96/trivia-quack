5db08a0b4a97413f0c21edc6ce4986b469e267bd
Elwin <elwin.carlos09@gmail.com>
2021-04-26 16:17:36 +0100

    comment out createRoom logic

--- client/src/layout/NewGame/index.js
+++ client/src/layout/NewGame/index.js
@@ -5,17 +5,17 @@ import { useHistory } from 'react-router-dom';
 const NewGame = () => {
     const history = useHistory();
 
-    const createRoom = async () => {
-        try {
-            let { data } = await axios.post("http.localhost:3000/games");
-            console.log(data);
-            history.push(`/game/${data.data.insertedId}`)     
-        } catch (err) {
-            console.warn(err);
-        }
-    }
+    // const createRoom = async () => {
+    //     try {
+    //         let { data } = await axios.post("http://localhost:3000/games");
+    //         console.log(data);
+    //         history.push(`/game/${data.id}`)     
+    //     } catch (err) {
+    //         console.warn(err);
+    //     }
+    // }
 
-    return <button onClick={createRoom} style={{backgroundColor: "transparent", borderStyle: "none", color: "white"}}>New Game</button>
+    return <a href="/create" className="new-game-button" >New Game</a>
 }
 
 export default NewGame;
