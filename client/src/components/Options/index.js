65593b9137727cd151bf7866c752ae0585bf6e2e
“AdilIqbal95” <adil.iqbal@hotmail.co.uk>
2021-04-26 16:51:47 +0100

    Return buttons in options component

---
+++ client/src/components/Options/index.js
@@ -0,0 +1,20 @@
+import React, {useState} from 'react';
+
+
+function Options () {
+
+
+
+  return (
+    <>
+      <button>Option1</button>
+      <button>Option2</button>
+      <button>Option3</button>
+      <button>Option4</button>
+    </>  
+    
+
+
+  )
+
+}
