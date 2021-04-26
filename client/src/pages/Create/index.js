7e282d19a2cf321d0f00c21ebc953537ec478a73
Elwin <elwin.carlos09@gmail.com>
2021-04-26 17:55:17 +0100

    add prop for creating a new game room

--- client/src/pages/Create/index.js
+++ client/src/pages/Create/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+
 import { CreateForm } from '../../components';
 
 const Lobby = () => {
+  const history = useHistory();
+
+  const createRoom = async () => {
+      try {
+          let { data } = await axios.post("http://localhost:3000/games");
+          console.log(data);
+          history.push(`/game/${data}`)     
+      } catch (err) {
+          console.warn(err);
+      }
+  }
+
   return (
     <main id="create" className="container">
-      <CreateForm />
+      <CreateForm createRoom={createRoom}/>
     </main>
   );
 };
