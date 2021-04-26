18e2204d2dd7dc09877d50c03aecd9c3a6e376ce
HeyAero <aeroyts@icloud.com>
2021-04-26 11:34:43 +0100

    add planet images for homepage and update styling for them

--- client/src/pages/Homepage/index.js
+++ client/src/pages/Homepage/index.js
@@ -4,6 +4,8 @@ const Homepage = () => {
   return (
     <main id="homepage">
       <h1>Trivia Quack</h1>
+      <img className="planet-left" src="../../images/planet-1.png" alt="Blue Planet" />
+      <img className="planet-right" src="../../images/planet-2.png" alt="Orange Planet" />
     </main>
   );
 };
