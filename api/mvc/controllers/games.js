a30db43fa8337446c08f05df070e72f28e4345d6
Elwin <elwin.carlos09@gmail.com>
2021-04-26 11:17:53 +0100

    add games controllers

---
+++ api/mvc/controllers/games.js
@@ -0,0 +1,38 @@
+const Game = require('../models/Game');
+
+const getAll = async(req, res) => {
+    try {
+        const games = await Game.all
+        res.status(200).json({data: games})
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: err })
+    }    
+};
+
+const getById = async (req, res) => {
+    try {
+        const id = req.params.id
+        const game = await Game.findById(id)
+        res.status(200).json({data: game})
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: err })
+    }
+};
+
+const postGame = async (req, res) => {
+    try {
+        const game = await Game.create()
+        res.status(200).json({data: game})
+    } catch(err) {
+        console.error(err);
+        res.status(500).json({ error: err })
+    }
+};
+
+
+
+
+
+module.exports = { getAll, getById, postGame };
