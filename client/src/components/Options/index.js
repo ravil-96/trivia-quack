fe9e4c3baabe2aca7be57fc285d1df9d22a5e026
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-27 12:53:39 +0100

    updating questions reducer and options component

--- client/src/components/Options/index.js
+++ client/src/components/Options/index.js
@@ -1,10 +1,21 @@
 import React, {useState} from 'react';
-
+import useParams from 'react-router-dom';
+import {useSelector, useDispatch} from 'react-redux';
+import {getQuestion} from '../../actions/questionActions';
 
 function Options () {
-
+  const {id} = useParams();
+  const dispatch = useDispatch();
 // 1. The user clicks on a button and it is highlighted
 // 2. The user submits an answer
+  dispatch(getQuestion(id))
+  
+  const question = useSelector(state => state.questions[0].questionTitle)
+  const option1 = useSelector(state => state.questions[0].option1)
+  const option2 = useSelector(state => state.questions[0].option2)
+  const option3 = useSelector(state => state.questions[0].option3)
+  const option4 = useSelector(state => state.questions[0].option4)
+
 
   const [selectedOption, setSelectedOption] = useState();
   
@@ -12,23 +23,20 @@ function Options () {
         const input = e.target.value
         setSelectedOption(input)
         console.log(input)
-        return input
     }
 
   const handleSubmit = e => {
         e.preventDefault();
-        submitAnswer(input)
+        submitAnswer(selectedOption)
     }
-
-  
 
   return (
     <>
-      <button onClick={handleSelect} value='option1'>Option1</button>
-      <button onClick={handleSelect} value='option2'>Option2</button>
-      <button onClick={handleSelect} value='option3'>Option3</button>
-      <button onClick={handleSelect} value='option4'>Option4</button>
-      <button type='submit'>Submit</button>
+      <button onClick={handleSelect} value={option1}>{option1}</button>
+      <button onClick={handleSelect} value={option2}>{option2}</button>
+      <button onClick={handleSelect} value={option3}>{option3}</button>
+      <button onClick={handleSelect} value={option4}>{option4}</button>
+      <button type='submit' onClick={handleSubmit}>Submit</button>
     </>  
 
   )
