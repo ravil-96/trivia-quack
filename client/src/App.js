691690022732c6d5ea6f5ba83b5a145d6fae446e
Elwin <elwin.carlos09@gmail.com>
2021-04-27 15:33:40 +0100

    fix merge conflicts with master staging branch

--- client/src/App.js
+++ client/src/App.js
@@ -15,17 +15,13 @@ const App = () => {
                 <Route path="/lobby">
                     <Lobby />
                 </Route>
-<<<<<<< HEAD
                 <Route path="/game/:id">
                     <GameRoom />
                 </Route>
                 <Route path="/questions">
                     <Questions />
                 </Route>
-                <Route path="/result/:id">
-=======
                 <Route path="/results/:id">
->>>>>>> 12c526f78bc618bc4959321e339c02e3e0b3c3c0
                     <Result />
                 </Route>
             </Switch>
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,5 @@
 export { default as CreateForm } from './CreateForm' 
-<<<<<<< HEAD
 export { default as Chat } from './Chat';
 export { default as ChatBox } from './ChatBox';
 export { default as Options } from './Options';
-=======
 export { default as PlayerCard } from './PlayerCard' 
->>>>>>> 12c526f78bc618bc4959321e339c02e3e0b3c3c0
