fef5c5e8835f348f07a9d590b1bb045ff6e6fc03
HeyAero <aeroyts@icloud.com>
2021-04-27 15:36:30 +0100

    add 1st-3rd place view for ScoreView and update styling on Highscore page

--- client/src/components/ScoreView/index.js
+++ client/src/components/ScoreView/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ScoreView = ({players}) => {
   return (
-    <div className="scoreview">
+    <div className="scoreview text-center">
       <div class="row">
         { players[1] ?
           <div class="second-place col-sm-4">
             <img src={players[1].icon} alt="Player Icon" />
-            <span>2nd</span>
-            <h5>{players[1].player}</h5>
+            <span>2</span>
+            <h5>Guest-{players[1].player}</h5>
             <p>{players[1].score}</p>
           </div>
           :
@@ -17,18 +17,18 @@ const ScoreView = ({players}) => {
         { players[0] ?
           <div class="first-place col-sm-4">
             <img src={players[0].icon} alt="Player Icon" />
-            <span>1st</span>
-            <h5>{players[0].player}</h5>
+            <span>1</span>
+            <h5>Guest-{players[0].player}</h5>
             <p>{players[0].score}</p>
           </div>
           :
           <></>
         }
         { players[2] ?
-          <div class="first-place col-sm-4">
+          <div class="third-place col-sm-4">
             <img src={players[2].icon} alt="Player Icon" />
-            <span>3st</span>
-            <h5>{players[2].player}</h5>
+            <span>3</span>
+            <h5>Guest-{players[2].player}</h5>
             <p>{players[2].score}</p>
           </div>
           :
