c6fce031af182ef3ad316771106cf9f8f5c0f6c2
HeyAero <aeroyts@icloud.com>
2021-04-27 15:13:38 +0100

    update ScoreView to include content with test data and update HighScore to use ScoreView

--- client/src/components/ScoreView/index.js
+++ client/src/components/ScoreView/index.js
@@ -1,9 +1,40 @@
 import React from 'react';
 
-const ScoreView = () => {
+const ScoreView = ({players}) => {
   return (
     <div className="scoreview">
-      <h1>Scoreview</h1>
+      <div class="row">
+        { players[1] ?
+          <div class="second-place col-sm-4">
+            <img src={players[1].icon} alt="Player Icon" />
+            <span>2nd</span>
+            <h5>{players[1].player}</h5>
+            <p>{players[1].score}</p>
+          </div>
+          :
+          <></>
+        }
+        { players[0] ?
+          <div class="first-place col-sm-4">
+            <img src={players[0].icon} alt="Player Icon" />
+            <span>1st</span>
+            <h5>{players[0].player}</h5>
+            <p>{players[0].score}</p>
+          </div>
+          :
+          <></>
+        }
+        { players[2] ?
+          <div class="first-place col-sm-4">
+            <img src={players[2].icon} alt="Player Icon" />
+            <span>3st</span>
+            <h5>{players[2].player}</h5>
+            <p>{players[2].score}</p>
+          </div>
+          :
+          <></>
+        }
+      </div>
     </div>
   )
 }
--- client/src/pages/Highscore/index.js
+++ client/src/pages/Highscore/index.js
@@ -1,9 +1,43 @@
 import React from 'react';
+import { ScoreView } from '../../components';
+
+import icon1 from '../../images/player-1.png';
+import icon2 from '../../images/player-2.png';
+import icon3 from '../../images/player-3.png';
+import icon4 from '../../images/player-4.png';
+import icon5 from '../../images/player-5.png';
+import icon6 from '../../images/player-6.png';
+import icon7 from '../../images/player-7.png';
+import icon8 from '../../images/player-8.png';
+import icon9 from '../../images/player-9.png';
+import icon10 from '../../images/player-10.png';
+
+const fakePlayers = ["123123", "213424", "234234", "234345"];
+const icons = [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9, icon10];
+
+const players = [
+  {
+    player: "123",
+    icon: icon1,
+    score: "95%",
+  },
+  {
+    player: "124",
+    icon: icon2,
+    score: "90%",
+  },
+  {
+    player: "125",
+    icon: icon3,
+    score: "85%",
+  }
+]
 
 const Highscore = () => {
   return (
-    <main id="highscore">
+    <main id="highscore" className="container">
       <h1>Highscores</h1>
+      <ScoreView players={players}/>
     </main>
   );
 }
