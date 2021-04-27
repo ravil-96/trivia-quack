e67ab8ea003952dc46d241934f9bd9fcd29c19b4
Elwin <elwin.carlos09@gmail.com>
2021-04-27 15:27:59 +0100

    reformat payload

--- client/src/features/questions/questionsSlice.js
+++ client/src/features/questions/questionsSlice.js
@@ -1,26 +1,25 @@
-const initialState = [
-    { 
-        questionId: 1, 
+const initialState = { 
+        // questionId: 1, 
         questionTitle: 'Who won Oscar for best actor in leading role in 1994',
-        option1: 'Tom Hanks', 
-        option2: 'Daniel Day-Lewis',
-        option3: 'Anthony Hopkins', 
-        option4: 'Liam Neeson',
+        options: ['Tom Hanks', 'Daniel Day-Lewis','Anthony Hopkins', 'Liam Neeson'],
         error: null,
-    }
-]
-
-function nextQuestionId(questions) {
-    const maxId = questions.reduce((maxId, question) => Math.max(question.id, maxId), 0)
-    return maxId + 1
 }
+
+// function nextQuestionId(questions) {
+//     const maxId = questions.reduce((maxId, question) => Math.max(question.id, maxId), 0)
+//     return maxId + 1
+// }
 
 export default function questionsReducer(state=initialState, action) {
     switch(action.type) {
         case 'game/moveToTheNextQuestion':
-            return [ ...state, action.payload ]
+            return { 
+                ...state, 
+                questionTitle: action.payload.questionTitle,
+                options: action.payload.possAnswers 
+            }
         case 'SET_ERROR':
-            return [...state, {...state, error: action.payload} ]
+            return {...state, error: action.payload}
         default:
             return state
     }
