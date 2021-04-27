9265cf8e2b497b82863c95ad2c58ffe09fd2085e
chris westerduin <chriswesterduin@outlook.com>
2021-04-27 11:25:02 +0100

    start work on integration tests

--- api/tests/integration/config.js
+++ api/tests/integration/config.js
@@ -1,21 +1,26 @@
 const request = require('supertest');
 const fs = require("fs");
-const { Pool } = require('pg');
+const { MongoClient } = require('mongodb')
 const app = require('../../server.js');
-
-const testSeed = fs.readFileSync(__dirname + '/test_seeds.sql').toString();
+const connectionUrl = process.env.DB_CONNECTION;
+const dbName = process.env.DB_NAME
+const testSeed = fs.readFileSync(__dirname + '/test_seeds.js').toString();
 
 const resetTestDB = () => {
-    return new Promise (async (resolve, reject) => {
-        try {
-            const db = new Pool()
-            await db.query(testSeed);
-            resolve('Test DB reset');
-        } catch (err) {
-            reject(`Test DB could not be reset: ${err} in ${err.file}`);
-        };
-    });
-}
+  return new Promise(async (resolve, reject) => {
+    try {
+      let db = await MongoClient.connect(connectionUrl);
+      console.log("connected to database!", dbName);
+      console.log(db)
+      await db.collection('games').insertMany([
+        {greeting: 'hello'}
+    ]);
+      resolve(client + "Test DB reset");
+    } catch (err) {
+      reject(`Test DB could not be reset: ${err} in ${err.file}`);
+    }
+  });
+};
 
 global.request = request;
 global.app = app;
--- api/tests/integration/integration.spec.js
+++ api/tests/integration/integration.spec.js
@@ -1,17 +1,26 @@
-// describe("integration tests", () => {
-//   let api;
-//   beforeEach(async () => {
-//     await resetTestDB();
-//   });
+describe("integration tests", () => {
+  let api;
+  beforeEach(async () => {
+    await resetTestDB();
+  });
 
-//   beforeAll(async () => {
-//     api = app.listen(5000, () =>
-//       console.log("Test server running on port 5000")
-//     );
-//   });
+  beforeAll(async () => {
+    api = app.listen(5000, () =>
+      console.log("Test server running on port 5000")
+    );
+  });
 
-//   afterAll((done) => {
-//     console.log("Gracefully stopping test server");
-//     api.close(done);
-//   })
-// })
+  afterAll((done) => {
+    console.log("Gracefully stopping test server");
+    api.close(done);
+  })
+
+  describe("game routes", () => {
+    it("should return a list of all habits", async () => {
+      const res = await request(api)
+        .get("/game")
+      expect(res.statusCode).toEqual(200);
+    });
+})
+
+})
