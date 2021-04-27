c91374033c76573c8b1bd21116946a65159eeb30
HeyAero <aeroyts@icloud.com>
2021-04-27 15:01:42 +0100

    add ScoreView component and update imports

---
+++ client/src/components/ScoreView/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ScoreView = () => {
+  return (
+    <div className="scoreview">
+      <h1>Scoreview</h1>
+    </div>
+  )
+}
+
+export default ScoreView;
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,3 @@
 export { default as CreateForm } from './CreateForm' 
 export { default as PlayerCard } from './PlayerCard' 
+export { default as ScoreView } from './ScoreView' 
