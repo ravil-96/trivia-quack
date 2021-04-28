5cae96280b676186eca620f5ccfc88442a9048ce
Elwin <elwin.carlos09@gmail.com>
2021-04-28 12:15:40 +0100

    create actions for paramsReducer

---
+++ client/src/actions/paramsActions/index.js
@@ -0,0 +1,3 @@
+export const addAmount = amount => ({type: 'ADD_AMOUNT', payload: amount}); 
+export const addCategory = category => ({type: 'ADD_CATEGORY', payload: category}); 
+export const addDiffculty= difficulty => ({type: 'ADD_DIFFICULTY', payload: difficulty});
