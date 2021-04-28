6397843bcad0f47011c6531f26495a5ce7f5edf4
Elwin <elwin.carlos09@gmail.com>
2021-04-28 11:54:37 +0100

    remove game room reference and stub out post request for questions

--- client/src/pages/Create/index.js
+++ client/src/pages/Create/index.js
@@ -3,14 +3,13 @@ import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
 import { CreateForm } from '../../components';
-import { GameRoom } from '../../layout';
 
 const Lobby = () => {
   const history = useHistory();
 
   const createRoom = async () => {
       try {
-          let { data } = await axios.post("http://localhost:3000/games?amount=4");
+          let { data } = await axios.post(`http://localhost:3000/games?amount=${amount}&category=${category}&difficulty=${difficulty}`);
           console.log(data);
           history.push(`/lobby/${data}`)     
       } catch (err) {
