cc74d1a52a96ff54da656cfb4ca2db0c4e872a5a
Elwin <elwin.carlos09@gmail.com>
2021-04-28 15:57:29 +0100

    remove paramsReducer

--- client/src/reducer.js
+++ client/src/reducer.js
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
 
 import myReducer from './features/myReducer';
-import paramsReducer from './features/paramsReducer';
-
-
 
 const rootReducer = combineReducers({
-      myReducer,
-      paramsReducer
+      myReducer
 })
 
 
