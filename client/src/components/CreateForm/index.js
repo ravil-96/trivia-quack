a2e03a140cf7fec2272b90f95d11fbdb42a2d976
Elwin <elwin.carlos09@gmail.com>
2021-04-28 12:58:14 +0100

    stub out event handlers for updating params store

--- client/src/components/CreateForm/index.js
+++ client/src/components/CreateForm/index.js
@@ -1,6 +1,30 @@
 import React, { useState } from 'react';
 
+import { useDispatch } from 'react-redux';
+import { addAmount, addCategory, addDiffculty } from '../../actions/paramsActions';
+
 const CreateForm = ({ createRoom }) => {
+
+  const [ amount , setAmount ] = useState("");
+  const [ category , setCategory ] = useState("");
+  const [ difficulty , setDifficulty ] = useState("");
+
+  const dispatch = useDispatch();
+
+  const selectAmount = (e) => {
+    setAmount(e.target.value);
+    dispatch(addAmount(amount));
+  }
+
+  const selectCategory = (e) => {
+    setCategory(e.target.value)
+    dispatch(addCategory(category));
+  }
+
+  const selectDifficulty = (e) => {
+    setDifficulty(e.target.value)
+    dispatch(addDifficulty(difficulty));
+  }
 
   const [showOption1, setShowOption1] = useState(true);
   const [showOption2, setShowOption2] = useState(false);
@@ -26,10 +50,10 @@ const CreateForm = ({ createRoom }) => {
     <form className="text-center" onSubmit={handleSubmit}>
       <div id="option-1" className={ showOption1 ? "" : "d-none"}>
           <label for="category">Select a Category for your quiz:</label>
-          <select name="category" id="category">
-            <option value="9">General Knowledge</option>
-            <option value="21">Sports</option>
-            <option value="26">Celebrities</option>
+          <select name="category" id="category" >
+            <option value="9" onSelect={selectCategory}>General Knowledge</option>
+            <option value="21" onSelect={selectCategory}>Sports</option>
+            <option value="26" onSelect={selectCategory}>Celebrities</option>
           </select>
           <a className="next-button" onClick={switchOption2}>Next</a>
       </div>
@@ -41,9 +65,9 @@ const CreateForm = ({ createRoom }) => {
       <div id="option-3" className={ showOption3 ? "" : "d-none"}>
         <label for="difficulty">Enter the difficulty:</label>
         <select name="difficulty" id="difficulty">
-          <option value="easy">Easy</option>
-          <option value="medium">Difficult</option>
-          <option value="hard">Hard</option>
+          <option value="easy" onSelect={selectDifficulty}>Easy</option>
+          <option value="medium" onSelect={selectDifficulty}>Difficult</option>
+          <option value="hard" onSelect={selectDifficulty}>Hard</option>
         </select>
       </div>
       <input type="submit" value="Submit" className={ showOption3 ? "" : "d-none"}/>
