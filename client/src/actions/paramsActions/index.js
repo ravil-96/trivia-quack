4ebaa7783414896f3f8ed8664022c03d53f7719e
Elwin <elwin.carlos09@gmail.com>
2021-04-28 15:58:03 +0100

    remove params reducer

--- client/src/actions/paramsActions/index.js
+++
@@ -1,3 +0,0 @@
-export const addAmount = amount => ({type: 'ADD_AMOUNT', payload: amount}); 
-export const addCategory = category => ({type: 'ADD_CATEGORY', payload: category}); 
-export const addDiffculty= difficulty => ({type: 'ADD_DIFFICULTY', payload: difficulty});
