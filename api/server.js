0a3dd0e9007ccbffa50d09ecedfd4f6a7b485b4b
Elwin <elwin.carlos09@gmail.com>
2021-04-28 08:21:12 +0100

    send welcome message on root route

--- api/server.js
+++ api/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/', (req, res) => console.log("Welcome to the Express server for Trivia Quack!"))
+app.get('/', (req, res) => res.send("Welcome to the Express server for Trivia Quack!"));
 
 const gamesController = require("./controllers/games")
 app.use('/games', gamesController)
