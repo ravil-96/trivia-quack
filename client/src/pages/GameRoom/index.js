9fd709b39350263098a0045a954b07d234556add
HeyAero <aeroyts@icloud.com>
2021-04-29 16:20:02 +0100

    update GameRoom to use getIcon function instead of importing directly

--- client/src/pages/GameRoom/index.js
+++ client/src/pages/GameRoom/index.js
@@ -1,14 +1,3 @@
-import icon1 from '../../images/player-1.png';
-import icon2 from '../../images/player-2.png';
-import icon3 from '../../images/player-3.png';
-import icon4 from '../../images/player-4.png';
-import icon5 from '../../images/player-5.png';
-import icon6 from '../../images/player-6.png';
-import icon7 from '../../images/player-7.png';
-import icon8 from '../../images/player-8.png';
-import icon9 from '../../images/player-9.png';
-import icon10 from '../../images/player-10.png';
-
 import React, { useState, useEffect } from'react';
 import { useParams, useHistory } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -17,6 +6,8 @@ import axios from 'axios';
 import { PlayerCard, Options } from '../../components'
 import { getAnswers, allNotReady } from '../../actions'
 import { API_ADDRESS } from '../../actions/globalVars';
+
+import { getIcon } from '../../actions/getIcon';
 
 
 const GameRoom = () => {
@@ -67,18 +58,9 @@ const GameRoom = () => {
         setTheme("theme-planet-1");
       }
     },[currentQuestion])
-
-    const icons = [icon1, icon2, icon3, icon4, icon5, icon6, icon7, icon8, icon9, icon10];
-
-    const returnIcon = () => {
-      let icon = icons[Math.floor(Math.random() * icons.length)];
-      return icon;
-    }
-  
-    const readyMarker = false;
   
     const returnPlayer = currentPlayers.map((player, index) => {
-        return <PlayerCard key={index} player={player.player} me={player.player === socket.socket.id} icon={returnIcon()} ready={player.ready} />
+        return <PlayerCard key={index} player={player.player} me={player.player === socket.socket.id} icon={getIcon()} ready={player.ready} />
     });
   
 
