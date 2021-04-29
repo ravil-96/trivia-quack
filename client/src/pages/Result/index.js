ef8afe2c06afc4acc1c552066afb9c35960a2706
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-29 17:25:30 +0100

    adding testing to JoinGame component

--- client/src/pages/Result/index.js
+++ client/src/pages/Result/index.js
@@ -3,15 +3,18 @@ import { useSelector } from 'react-redux'
 import axios from 'axios'
 import { useParams } from 'react-router-dom'
 import { ScoreView } from '../../components'
-import { API_ADDRESS } from '../../actions/globalVars';
+import { API_Local, API_Production } from '../../actions/globalVars';
 
 const Result = () => {
 
     const socket = useSelector(state => state.myReducer.socket)
+    const qType = useSelector(state => state.myReducer.questions[0])
+    //  const socket = useSelector(state => state.myReducer.socket)
     const renderHTML = (rawHTML) => React.createElement("div", { dangerouslySetInnerHTML: { __html: rawHTML } });
     const { id } = useParams()
     const [results, setResults] = useState([])
     const [scores, setScores] = useState([])
+    const [points, setPoints] = useState([])
     const [loading, setLoading] = useState()
     const [error, setError] = useState(null)
     const [showResults, setShowResults] = useState(true);
@@ -21,11 +24,14 @@ const Result = () => {
         async function getResults() {
           try {
             setLoading(true)
-            let { data } = await axios.get(`${API_ADDRESS}/games/${id}/results`);
+            let { data } = await axios.get(`${API_Production}/games/${id}/results`);
             setResults(data.data)
-            const scoreSortFix = data.scores.sort((a,b) => b.count - a.count).map(p => ({name: p.name, count: p.count}))
-            setScores(scoreSortFix)
+            setScores(data.scores)
+            console.log(data.scores)
+            console.log(data.data)
             setLoading(false)
+            console.log('This is the data:',data)
+            console.log('questions from reducer: ',qType)
           } catch (err) {
             setLoading(false)
             setError(err)
@@ -35,25 +41,29 @@ const Result = () => {
         getResults();
       }, []);
 
+      const scoreSort = scores.sort((a,b) => b.score - a.score)
+      const scoreList = scores.map((score, i) => <li key={i}>{score.name}: {score.count}</li>)
+
       const answersList = results.map((result, i) => {
         return (
-          <div key={i} className="answer-box">
-            <div className="answer-question">{renderHTML(result.question)}</div>
+          <div key={i}>
+            <div>{renderHTML(result.question)}</div>
             <ul>
               {result.all_answers.map((answer, j) => (
                 <li
-                  className={
-                      answer === result.correct_answer ? "answer-correct" : "answer-wrong"
-                  }
+                  style={{
+                    background:
+                      answer === result.correct_answer ? "green" : "red",
+                  }}
                 >
                   {renderHTML(answer)}
-                  <ul className="answer-player-list">{result.player_answers.filter(c => c.answer === answer).map(d => <li>{d.player}</li>)}</ul>
+                  <ul>{result.player_answers.filter(c => c.answer === answer).map(d => <li>{d.player}</li>)}</ul>
                 </li>
               ))}
             </ul>
           </div>
         );
-      });
+      });      
 
       const handleResult = () => {
         setAnswersResults(false);
@@ -73,20 +83,25 @@ const Result = () => {
         <div>{JSON.stringify(error)}</div>
       ) : (
         <>
-          <div className="switch-buttons">
-            <a className="switch-left" onClick={handleResult}>
+          <div class="switch-buttons">
+            <a onClick={handleResult}>
               Results
             </a>
-            <a className="switch-right" onClick={handleAnswers}>
+            <a onClick={handleAnswers}>
               Answers
             </a>
           </div>
           <div className={showResults ? "scores" : "d-none"}>
             <h1>Results</h1>
-            <ScoreView players={scores}/>
+            <ul>{scoreList.sort((a,b) => b.score - a.score)}</ul>
+            <ScoreView players={scoreSort}/>
           </div>
           <div className={showAnswers ? "answers" : "d-none"}>
-            <div className="answer-holder">{answersList}</div>
+            <div>{answersList}</div>
+
+            <div>
+              <p>{points}</p>
+            </div>
           </div>     
 
         </>
