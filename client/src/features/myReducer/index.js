6577918eb738b4788940c3c37692e2d57a0e74d4
Elwin <elwin.carlos09@gmail.com>
2021-04-29 17:12:22 +0100

    change reducer to arrow function

--- client/src/features/myReducer/index.js
+++ client/src/features/myReducer/index.js
@@ -1,4 +1,4 @@
-function myReducer (state = {players: [], answers: []}, action) {
+const myReducer = (state = {players: [], answers: []}, action) => {
     switch (action.type) {
       case 'ADD_SOCKET':
         return ({
@@ -10,20 +10,21 @@ function myReducer (state = {players: [], answers: []}, action) {
             ...state,
             players: action.payload.map(player => ({player: player, ready: false}))
         })
-          case 'PLAYER_READY':
-            const playerToToggle = state.players.find(p => p.player === action.payload)
-            const likeIdx = state.players.indexOf(playerToToggle)
-            const updatedPlayers = [ 
-                ...state.players.slice(0, likeIdx),
-                { ...playerToToggle, ready: !playerToToggle.ready },
-                ...state.players.slice(likeIdx+1)
-            ]
-            return { ...state, players: updatedPlayers }
-            case 'ALL_NOT_READY':
-              const unReadyPlayers = [ 
-                ...state.players.map(p => ({player: p.player, ready: false}))
-              ]
-              return {
+      case 'PLAYER_READY':
+          const playerToToggle = state.players.find(p => p.player === action.payload)
+          const likeIdx = state.players.indexOf(playerToToggle)
+          const updatedPlayers = [ 
+              ...state.players.slice(0, likeIdx),
+              { ...playerToToggle, ready: !playerToToggle.ready },
+              ...state.players.slice(likeIdx+1)
+          ]
+          return { ...state, players: updatedPlayers }
+          
+      case 'ALL_NOT_READY':
+        const unReadyPlayers = [
+          ...state.players.map(p => ({player: p.player, ready: false}))
+        ]
+        return {
                 ...state, players: unReadyPlayers 
               }
       case 'LOAD_QUESTIONS':
@@ -36,11 +37,8 @@ function myReducer (state = {players: [], answers: []}, action) {
           ...state,
           answers: state.answers.concat([action.payload])
         })
-      case 'SET_ERROR' :
+      case 'SET_ERROR':
         return 'error'
-      case 'ADD_MESSAGE':
-        console.log(action.payload)
-        return state
       default:
         return state
     }
